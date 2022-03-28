55fdec773d92abfb162faa5df30e92a788c43ed4
Benjamin Kobjolke <b.kobjolke@xida.de>
2022-03-28 15:39:04 +0200

    FEATURE: nano version

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # arduino-nfc-mp3-player
+
+## Required libraries
+- https://github.com/Seeed-Studio/PN532
+- https://github.com/don/NDEF
+- https://github.com/DFRobot/DFRobotDFPlayerMini
+
+
+### Links
+- https://electropeak.com/learn/interfacing-pn532-nfc-rfid-reader-writer-module-i2c-spi-with-arduino/
+- https://wiki.dfrobot.com/DFPlayer_Mini_SKU_DFR0299
+- https://github.com/Makuna/DFMiniMp3/wiki/API-Reference
+- https://funduino.de/nr-43-dfplayer-mini-mp3-player
+
+
+
+
+
---
+++ arduino-due-version/nfcmp3player/nfcmp3player/nfcmp3player.ino
@@ -0,0 +1,154 @@
+//#include <SoftwareSerial.h>
+#include <DFMiniMp3.h>
+
+// implement a notification class,
+// its member methods will get called 
+//
+class Mp3Notify
+{
+public:
+  static void PrintlnSourceAction(DfMp3_PlaySources source, const char* action)
+  {
+    if (source & DfMp3_PlaySources_Sd) 
+    {
+        Serial.print("SD Card, ");
+    }
+    if (source & DfMp3_PlaySources_Usb) 
+    {
+        Serial.print("USB Disk, ");
+    }
+    if (source & DfMp3_PlaySources_Flash) 
+    {
+        Serial.print("Flash, ");
+    }
+    Serial.println(action);
+  }
+  static void OnError(uint16_t errorCode)
+  {
+    // see DfMp3_Error for code meaning
+    Serial.println();
+    Serial.print("Com Error ");
+    Serial.println(errorCode);
+  }
+  static void OnPlayFinished(DfMp3_PlaySources source, uint16_t track)
+  {
+    Serial.print("Play finished for #");
+    Serial.println(track);  
+  }
+  static void OnPlaySourceOnline(DfMp3_PlaySources source)
+  {
+    PrintlnSourceAction(source, "online");
+  }
+  static void OnPlaySourceInserted(DfMp3_PlaySources source)
+  {
+    PrintlnSourceAction(source, "inserted");
+  }
+  static void OnPlaySourceRemoved(DfMp3_PlaySources source)
+  {
+    PrintlnSourceAction(source, "removed");
+  }
+};
+
+#include "PN532_SWHSU.h"
+
+#include <PN532.h>
+#include <NfcAdapter.h>
+
+PN532_SWHSU pn532swhsu( Serial2 );
+
+//PN532 nfc( pn532swhsu );
+NfcAdapter nfc = NfcAdapter(pn532swhsu);
+
+
+// forward declare the notify class, just the name
+//
+class Mp3Notify; 
+
+// define a handy type using serial and our notify class
+//
+typedef DFMiniMp3<HardwareSerial, Mp3Notify> DfMp3; 
+
+// instance a DfMp3 object, 
+//
+DfMp3 dfmp3(Serial1);
+
+// Some arduino boards only have one hardware serial port, so a software serial port is needed instead.
+// comment out the above definitions and use these
+//SoftwareSerial secondarySerial(10, 11); // RX, TX
+//typedef DFMiniMp3<SoftwareSerial, Mp3Notify> DfMp3;
+// DfMp3 dfmp3(secondarySerial);
+
+// implement a notification class,
+// its member methods will get called 
+//
+
+void setup(void) {
+  Serial.begin(115200);
+
+  Serial.println("initializing...");
+  
+  dfmp3.begin();
+  dfmp3.reset(); 
+  
+  // show some properties and set the volume
+  uint16_t volume = dfmp3.getVolume();
+  Serial.print("volume ");
+  Serial.println(volume);
+  dfmp3.setVolume(15);
+  
+  uint16_t count = dfmp3.getTotalTrackCount(DfMp3_PlaySource_Sd);
+  Serial.print("files ");
+  Serial.println(count);
+
+  uint16_t mode = dfmp3.getPlaybackMode();
+  Serial.print("playback mode ");
+  Serial.println(mode);
+  
+  Serial.println("starting...");  
+  //dfmp3.playRandomTrackFromAll();
+ 
+  
+  delay(2000);
+  nfc.begin();
+
+}
+
+void loop(void) {
+    Serial.println("\nScan a NFC tag\n");
+    if (nfc.tagPresent())
+    {
+        NfcTag tag = nfc.read();         
+        if(tag.hasNdefMessage()) {            
+            NdefMessage message = tag.getNdefMessage();
+            for(int i=0;i<message.getRecordCount();i++) {
+              NdefRecord record = message.getRecord(i);              
+              int payloadLength = record.getPayloadLength();
+              byte payload[payloadLength];
+              record.getPayload(payload);
+              String payloadAsString = "";
+              for (int c = 1; c < payloadLength; c++) {
+                payloadAsString += (char)payload[c];
+              }
+              //Serial.println(payloadAsString);
+
+              if (payloadAsString.equals("en0")) { 
+                  Serial.println(0);
+                   dfmp3.playMp3FolderTrack(1);
+              } else if(payloadAsString.equals("en1")) {                    
+                  Serial.println(1);
+                   dfmp3.playMp3FolderTrack(2);
+              } else if (payloadAsString.equals("en2")) {
+                  Serial.println(2);
+                   dfmp3.playMp3FolderTrack(3);
+              } else if (payloadAsString.equals("en3")) { 
+                  dfmp3.setVolume(30);                                 
+                  Serial.println(4);
+              } else if (payloadAsString.equals("en4")) {
+                  dfmp3.setVolume(15);             
+                  Serial.println(5);
+              } 
+            }        
+          }
+    }    
+    delay(1000);
+}
